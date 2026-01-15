d58aa8a0050f4f6dadb5e30f9799b91dda4e7192
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-15 21:11:03 +0530

    Update Turtle_race.py

--- Turtle_race.py
+++ Turtle_race.py
@@ -4,37 +4,41 @@ import random
 
 screen = Screen()
 screen.setup(width = 500, height = 400 )
-user_input = screen.textinput("Take your pick", " Choose your winning color : " )
-'''
-t4.penup()
-t4.goto(x = -230, y = 80 )
-t4.down()
 
-t5.penup()
-t5.goto(x = -230, y = -80 )
-t5.down()
-
-t6.penup()
-t6.goto(x = -230, y = -120 )
-t6.down()
-#print(user_input)
-'''
-a = -200
 start_y = -120
 spacing = 50
 
 turtles = []
+colors = [ "red", "orange", "blue", "black", "purple", "yellow", "violet" ]
+random.shuffle(colors)
+
 
 for turtle_index in range(0, 6):
     t = Turtle(shape = "turtle")
+    t.color(colors[turtle_index])
     t.penup()
     t.goto(-230, start_y + turtle_index * spacing)
     turtles.append(t)
-   
-    
+
+user_input = screen.textinput("Take your pick", " Choose your winning color : " )
+print(user_input)
+
+race_on = True
+winning_color = None
+
+while race_on is True:
+    for t in turtles:
+        t.forward(random.randint(1,10))
+        if t.xcor() >= 230:
+            winning_color = t.pencolor()
+            race_on = False
+            break
 
 
-    
 
+if user_input.lower() == winning_color.lower():
+    print( winning_color + " WON...The " + winning_color + " turtle is the WINNER   !!!!!!!!!! " )
+else:
+    print( user_input + " turtle lost. The " + winning_color + " turtle is the winner" ) 
 
-
+screen.exitonclick()
